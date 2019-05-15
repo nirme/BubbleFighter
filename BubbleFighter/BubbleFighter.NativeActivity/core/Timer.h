89f22191aaf38fb97c9fa6e874f1e8f372c9db53
Przemyslaw Brzoska <nirme.89@gmail.com>
2019-05-15 03:02:24 +0200

    .

--- BubbleFighter/BubbleFighter.NativeActivity/core/Timer.h
+++ BubbleFighter/BubbleFighter.NativeActivity/core/Timer.h
@@ -1,51 +1,125 @@
 #pragma once
 
 
-#include <jni.h>
-#include <sys/time.h>
+#include <list>
+#include <algorithm>
+
 #include <chrono>
 
-#include <time.h>
+//#include <time.h>
 
 
 class Timer
 {
+public: 
+	typedef std::chrono::steady_clock::time_point TimePoint;
+
 private:
 
-	typedef std::chrono::steady_clock::time_point TimePoint;
 
 	TimePoint timePoint[2];
 	unsigned short currentTimePoint;
 
 	double durationInSeconds;
 
+	bool paused;
+	std::list<TimeDuration*> durations;
 
-	Timer()
+public:
+
+	Timer() : paused(false)
 	{
 		timePoint[0] = std::chrono::steady_clock::now();
 
-		currentTimePoint = 1;
-		timePoint[currentTimePoint] = std::chrono::steady_clock::now();
+		if (!paused)
+			timePoint[1] = std::chrono::steady_clock::now();
+		else
+			timePoint[1] = timePoint[0];
 
+
+		currentTimePoint = 1;
 		durationInSeconds = std::chrono::duration<double>( timePoint[1] - timePoint[0]).count();
 	};
 
 
 	void update()
 	{
-		unsigned short prevTimePont(currentTimePoint);
-		currentTimePoint = ++currentTimePoint % (sizeof(timePoint) / sizeof(timePoint[0]));
-		durationInSeconds = std::chrono::duration<double>(timePoint[currentTimePoint] - timePoint[prevTimePont]).count();
+		//	struct timespec now;
+		//	clock_gettime(CLOCK_MONOTONIC, &now);
+		
+
+		if (!paused)
+		{
+			unsigned short prevTimePont(currentTimePoint);
+			currentTimePoint = ++currentTimePoint % (sizeof(timePoint) / sizeof(timePoint[0]));
+			durationInSeconds = std::chrono::duration<double>(timePoint[currentTimePoint] - timePoint[prevTimePont]).count();
+		}
 	};
+
+	//update the counter before the pause to have the most current change in time saved
+	void pause()
+	{
+		update();
+		paused = true;
+	};
+
+	void unpause()
+	{
+		timePoint[0] = std::chrono::steady_clock::now();
+		timePoint[1] = timePoint[0];
+		currentTimePoint = 1;
+		durationInSeconds = 0.0;
+
+		paused = true;
+	};
+
+	void removeDuration(TimeDuration* td) noexcept
+	{
+		//find the duration in list and remove it
+		auto it = std::find(durations.begin(), durations.end(), td);
+		if (it != durations.end())
+			durations.erase(it);
+	};
+
 
 };
 
-/*
-int tt()
-{
-	struct timespec now;
-	clock_gettime(CLOCK_MONOTONIC, &now);
-	long test = nanoTime();
-}
 
-*/
+class TimeDuration
+{
+private:
+	const Timer::TimePoint startingTime;
+	double durationInSeconds;
+
+	Timer* const timerUsed;
+
+public:
+
+	TimeDuration(Timer::TimePoint _startingTime, Timer* _timerUsed)
+		: startingTime(_startingTime), timerUsed(_timerUsed), durationInSeconds(0.0)
+	{};
+
+	~TimeDuration()
+	{
+		timerUsed->removeDuration(this);
+	};
+
+	void update(Timer::TimePoint currentTimePoint)
+	{
+		durationInSeconds = std::chrono::duration<double>(currentTimePoint - startingTime).count();
+	};
+
+	operator double()
+	{
+		return durationInSeconds;
+	};
+
+	operator float()
+	{
+		return (float) durationInSeconds;
+	};
+	
+};
+
+
+/**/
