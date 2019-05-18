448f976d18b19a489dadee70165381b189eb4a38
Przemyslaw Brzoska <nirme.89@gmail.com>
2019-05-18 20:39:51 +0200

    .

--- BubbleFighter/BubbleFighter.NativeActivity/core/Renderer.h
+++ BubbleFighter/BubbleFighter.NativeActivity/core/Renderer.h
@@ -20,16 +20,43 @@
 
 namespace Graphic
 {
+
+	inline bool glIsError(GLenum errNum)
+	{
+		return errNum != GL_NO_ERROR;
+	};
+
+
 	class Renderer
 	{
 
 	private:
 
-
+		virtual void logErrors(GLenum errNum) = 0;
 
 
 	public:
-		virtual PTexture2D loadTexture() = 0;
+		virtual PTexture2D loadTexture(const std::string& _textureName)
+		{
+			Texture2D* newTex = nullptr;
+			GLuint texId = -1;
+
+			glGenTextures(1, &texId);
+
+			void glTexParameteri(GLenum target, GLenum pname, GLint param);
+
+			void glTexImage2D(GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid * data);
+
+			GLenum glGetError(void);
+
+
+
+
+			//newTex = new Texture2D(_textureName,texId);
+			//newTex->setParameter
+		};
+
+
 		virtual void unloadTexture(Texture2D* texture) = 0;
 
 		virtual PShader loadShader() = 0;
